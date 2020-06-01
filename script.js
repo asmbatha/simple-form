afcaf1d0b207ef5a5bd5a7f5f77875737ca5822b
asmbatha <asmbatha.13@gmail.com>
2020-06-01 11:09:44 +0200

    first add

---
+++ script.js
@@ -0,0 +1,41 @@
+
+            window.onload = function(e){ 
+                const form = document.getElementById('form')
+                form.addEventListener('submit', function (e) {
+                    e.preventDefault()
+                    
+                    const name = form.name.value
+                    const email = form.email.value
+                    const countryList = form.countryList.value
+                    const message = form.message.value
+
+                    let errors = []
+                    
+                    // Name field must not be empty
+                    if (name == '') {
+                        errors.push('Name field must not be empty')
+                    }
+
+                    // Email address must not be empty and must be tested against the regular expression /^\S+@\S+\.\S+$/
+                    if (!/^\S+@\S+\.\S+$/.test(email)) {
+                        errors.push('Email field is invalid')
+                    }
+
+                    // Password and Confirm Password fields must not be empty
+                    if (countryList == '0') {
+                        errors.push('Please select a country');
+                    }
+					
+					// Message box must not be empty
+					if (message == ''){
+						errors.push('Message must not be empty')
+					}
+
+                    if (errors.length) {
+                        alert(errors.join("\n"))
+                    } else {
+                        alert('Registration was successful')
+                    }
+                })
+            }
+        
